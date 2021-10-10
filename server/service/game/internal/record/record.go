c1d3f77922b9d9c92caa70cd43bd13097c31c87b
alienspaces <alienspaces@gmail.com>
2021-10-10 20:20:39 +1100

    Add record definitions

--- server/service/game/internal/record/record.go
+++ server/service/game/internal/record/record.go
@@ -4,7 +4,112 @@ import (
 	"gitlab.com/alienspaces/go-mud/server/core/repository"
 )
 
-// Game -
-type Game struct {
+// Dungeon -
+type Dungeon struct {
+	Name        string `db:"name"`
+	Description string `db:"description"`
+	repository.Record
+}
+
+// DungeonLocation -
+type DungeonLocation struct {
+	DungeonID                  string `db:"dungeon_id"`
+	Name                       string `db:"name"`
+	Description                string `db:"description"`
+	Default                    bool   `db:"default"`
+	NorthDungeonLocationID     string `db:"north_dungeon_location_id"`
+	NortheastDungeonLocationID string `db:"northeast_dungeon_location_id"`
+	EastDungeonLocationID      string `db:"east_dungeon_location_id"`
+	SoutheastDungeonLocationID string `db:"southeast_dungeon_location_id"`
+	SouthDungeonLocationID     string `db:"south_dungeon_location_id"`
+	SouthwestDungeonLocationID string `db:"southwest_dungeon_location_id"`
+	WestDungeonLocationID      string `db:"west_dungeon_location_id"`
+	NorthwestDungeonLocationID string `db:"northwest_dungeon_location_id"`
+	UpDungeonLocationID        string `db:"up_dungeon_location_id"`
+	DownDungeonLocationID      string `db:"down_dungeon_location_id"`
+	repository.Record
+}
+
+type DungeonCharacter struct {
+	DungeonID         string `db:"dungeon_id"`
+	DungeonLocationID string `db:"dungeon_location_id"`
+	Name              string `db:"name"`
+	Strength          int    `db:"strength"`
+	Dexterity         int    `db:"dexterity"`
+	Intelligence      int    `db:"intelligence"`
+	Health            int    `db:"health"`
+	Fatigue           int    `db:"fatigue"`
+	Coins             int    `db:"coins"`
+	ExperiencePoints  int    `db:"experience_points"`
+	AttributePoints   int    `db:"attribute_points"`
+	repository.Record
+}
+
+type DungeonMonster struct {
+	DungeonID         string `db:"dungeon_id"`
+	DungeonLocationID string `db:"dungeon_location_id"`
+	Name              string `db:"name"`
+	Strength          int    `db:"strength"`
+	Dexterity         int    `db:"dexterity"`
+	Intelligence      int    `db:"intelligence"`
+	Health            int    `db:"health"`
+	Fatigue           int    `db:"fatigue"`
+	Coins             int    `db:"coins"`
+	ExperiencePoints  int    `db:"experience_points"`
+	AttributePoints   int    `db:"attribute_points"`
+	repository.Record
+}
+
+type DungeonObject struct {
+	DungeonID          string `db:"dungeon_id"`
+	DungeonLocationID  string `db:"dungeon_location_id"`
+	DungeonCharacterID string `db:"dungeon_character_id"`
+	DungeonMonsterID   string `db:"dungeon_monster_id"`
+	Name               string `db:"name"`
+	Description        string `db:"description"`
+	DescriptionLong    string `db:"description_long"`
+	IsStashed          bool   `db:"is_stashed"`
+	IsEquipped         bool   `db:"is_equipped"`
+	repository.Record
+}
+
+type DungeonAction struct {
+	DungeonID                              string `db:"dungeon_id"`
+	DungeonLocationID                      string `db:"dungeon_location_id"`
+	DungeonCharacterID                     string `db:"dungeon_character_id"`
+	DungeonMonsterID                       string `db:"dungeon_monster_id"`
+	SerialID                               string `db:"serial_id"`
+	ResolvedCommand                        string `db:"resolved_command"`
+	ResolvedEquippedDungeonObjectID        string `db:"resolved_equipped_dungeon_object_id"`
+	ResolvedEquippedDungeonObjectName      string `db:"resolved_equipped_dungeon_object_name"`
+	ResolvedStashedDungeonObjectID         string `db:"resolved_stashed_dungeon_object_id"`
+	ResolvedStashedDungeonObjectName       string `db:"resolved_stashed_dungeon_object_name"`
+	ResolvedTargetDungeonObjectID          string `db:"resolved_target_dungeon_object_id"`
+	ResolvedTargetDungeonObjectName        string `db:"resolved_target_dungeon_object_name"`
+	ResolvedTargetDungeonCharacterID       string `db:"resolved_target_dungeon_character_id"`
+	ResolvedTargetDungeonCharacterName     string `db:"resolved_target_dungeon_character_name"`
+	ResolvedTargetDungeonMonsterID         string `db:"resolved_target_dungeon_monster_id"`
+	ResolvedTargetDungeonMonsterName       string `db:"resolved_target_dungeon_monster_name"`
+	ResolvedTargetDungeonLocationDirection string `db:"resolved_target_dungeon_location_direction"`
+	ResolvedTargetDungeonLocationID        string `db:"resolved_target_dungeon_location_id"`
+	ResolvedTargetDungeonLocationName      string `db:"resolved_target_dungeon_location_name"`
+	repository.Record
+}
+
+type DungeonActionCharacter struct {
+	DungeonActionID    string `db:"dungeon_action_id"`
+	DungeonCharacterID string `db:"dungeon_character_id"`
+	repository.Record
+}
+
+type DungeonActionMonster struct {
+	DungeonActionID  string `db:"dungeon_action_id"`
+	DungeonMonsterID string `db:"dungeon_monster_id"`
+	repository.Record
+}
+
+type DungeonActionObject struct {
+	DungeonActionID string `db:"dungeon_action_id"`
+	DungeonObjectID string `db:"dungeon_object_id"`
 	repository.Record
 }
--- server/service/game/internal/server/runner/handler.go
+++ server/service/game/internal/server/runner/handler.go
@@ -12,8 +12,8 @@ import (
 	"gitlab.com/alienspaces/go-mud/server/service/game/internal/record"
 )
 
-// GetGamesHandler -
-func (rnr *Runner) GetGamesHandler(w http.ResponseWriter, r *http.Request, pp httprouter.Params, qp map[string]interface{}, l logger.Logger, m modeller.Modeller) {
+// GetDungeonHandler -
+func (rnr *Runner) GetDungeonHandler(w http.ResponseWriter, r *http.Request, pp httprouter.Params, qp map[string]interface{}, l logger.Logger, m modeller.Modeller) {
 
 	l.Info("** Get games handler ** p >%#v< m >%#v<", pp, m)
 
@@ -21,133 +21,14 @@ func (rnr *Runner) GetGamesHandler(w http.ResponseWriter, r *http.Request, pp ht
 	var err error
 
 	// Path parameters
-	id := pp.ByName("game_id")
+	id := pp.ByName("dungeon_id")
 
-	// Single resource
-	if id != "" {
-
-		l.Info("Getting game record ID >%s<", id)
-
-		rec, err := m.(*model.Model).GetGameRec(id, false)
-		if err != nil {
-			rnr.WriteModelError(l, w, err)
-			return
-		}
-
-		// Resource not found
-		if rec == nil {
-			rnr.WriteNotFoundError(l, w, id)
-			return
-		}
-
-		recs = append(recs, rec)
-
-	} else {
-
-		l.Info("Querying game records")
-
-		params := make(map[string]interface{})
-		for paramName, paramValue := range qp {
-			l.Info("Querying game records with param name >%s< value >%v<", paramName, paramValue)
-			params[paramName] = paramValue
-		}
-
-		recs, err = m.(*model.Model).GetGameRecs(params, nil, false)
-		if err != nil {
-			rnr.WriteModelError(l, w, err)
-			return
-		}
-	}
-
-	// Assign response properties
-	data := []schema.GameData{}
-	for _, rec := range recs {
-
-		// Response data
-		responseData, err := rnr.RecordToGameResponseData(rec)
-		if err != nil {
-			rnr.WriteSystemError(l, w, err)
-			return
-		}
-
-		data = append(data, responseData)
-	}
-
-	res := schema.GameResponse{
-		Data: data,
-	}
-
-	err = rnr.WriteResponse(l, w, res)
-	if err != nil {
-		l.Warn("Failed writing response >%v<", err)
-		return
-	}
-}
-
-// PostGamesHandler -
-func (rnr *Runner) PostGamesHandler(w http.ResponseWriter, r *http.Request, pp httprouter.Params, qp map[string]interface{}, l logger.Logger, m modeller.Modeller) {
-
-	l.Info("** Post games handler ** p >%#v< m >#%v<", pp, m)
-
-	// Path parameters
-	id := pp.ByName("game_id")
-
-	req := schema.GameRequest{}
-
-	err := rnr.ReadRequest(l, r, &req)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
+	if id == "" {
+		rnr.WriteNotFoundError(l, w, id)
 		return
 	}
 
-	rec := record.Game{}
-
-	// Assign request properties
-	rec.ID = id
-
-	// Record data
-	err = rnr.GameRequestDataToRecord(req.Data, &rec)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
-		return
-	}
-
-	err = m.(*model.Model).CreateGameRec(&rec)
-	if err != nil {
-		rnr.WriteModelError(l, w, err)
-		return
-	}
-
-	// Response data
-	responseData, err := rnr.RecordToGameResponseData(&rec)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
-		return
-	}
-
-	// Assign response properties
-	res := schema.GameResponse{
-		Data: []schema.GameData{
-			responseData,
-		},
-	}
-
-	err = rnr.WriteResponse(l, w, res)
-	if err != nil {
-		l.Warn("Failed writing response >%v<", err)
-		return
-	}
-}
-
-// PutGamesHandler -
-func (rnr *Runner) PutGamesHandler(w http.ResponseWriter, r *http.Request, pp httprouter.Params, qp map[string]interface{}, l logger.Logger, m modeller.Modeller) {
-
-	l.Info("** Put games handler ** p >%#v< m >#%v<", pp, m)
-
-	// Path parameters
-	id := pp.ByName("game_id")
-
-	l.Info("Updating resource ID >%s<", id)
+	l.Info("Getting game record ID >%s<", id)
 
 	rec, err := m.(*model.Model).GetGameRec(id, false)
 	if err != nil {
@@ -161,39 +42,24 @@ func (rnr *Runner) PutGamesHandler(w http.ResponseWriter, r *http.Request, pp ht
 		return
 	}
 
-	req := schema.GameRequest{}
-
-	err = rnr.ReadRequest(l, r, &req)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
-		return
-	}
-
-	// Record data
-	err = rnr.GameRequestDataToRecord(req.Data, rec)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
-		return
-	}
-
-	err = m.(*model.Model).UpdateGameRec(rec)
-	if err != nil {
-		rnr.WriteModelError(l, w, err)
-		return
-	}
-
-	// Response data
-	responseData, err := rnr.RecordToGameResponseData(rec)
-	if err != nil {
-		rnr.WriteSystemError(l, w, err)
-		return
-	}
+	recs = append(recs, rec)
 
 	// Assign response properties
-	res := schema.GameResponse{
-		Data: []schema.GameData{
-			responseData,
-		},
+	data := []schema.DungeonData{}
+	for _, rec := range recs {
+
+		// Response data
+		responseData, err := rnr.RecordToDungeonResponseData(rec)
+		if err != nil {
+			rnr.WriteSystemError(l, w, err)
+			return
+		}
+
+		data = append(data, responseData)
+	}
+
+	res := schema.DungeonResponse{
+		Data: data,
 	}
 
 	err = rnr.WriteResponse(l, w, res)
@@ -203,16 +69,63 @@ func (rnr *Runner) PutGamesHandler(w http.ResponseWriter, r *http.Request, pp ht
 	}
 }
 
-// GameRequestDataToRecord -
-func (rnr *Runner) GameRequestDataToRecord(data schema.GameData, rec *record.Game) error {
+// GetDungeonsHandler -
+func (rnr *Runner) GetDungeonsHandler(w http.ResponseWriter, r *http.Request, pp httprouter.Params, qp map[string]interface{}, l logger.Logger, m modeller.Modeller) {
+
+	l.Info("** Get games handler ** p >%#v< m >%#v<", pp, m)
+
+	var recs []*record.Game
+	var err error
+
+	l.Info("Querying game records")
+
+	params := make(map[string]interface{})
+	for paramName, paramValue := range qp {
+		l.Info("Querying game records with param name >%s< value >%v<", paramName, paramValue)
+		params[paramName] = paramValue
+	}
+
+	recs, err = m.(*model.Model).GetGameRecs(params, nil, false)
+	if err != nil {
+		rnr.WriteModelError(l, w, err)
+		return
+	}
+
+	// Assign response properties
+	data := []schema.DungeonData{}
+	for _, rec := range recs {
+
+		// Response data
+		responseData, err := rnr.RecordToDungeonResponseData(rec)
+		if err != nil {
+			rnr.WriteSystemError(l, w, err)
+			return
+		}
+
+		data = append(data, responseData)
+	}
+
+	res := schema.DungeonResponse{
+		Data: data,
+	}
+
+	err = rnr.WriteResponse(l, w, res)
+	if err != nil {
+		l.Warn("Failed writing response >%v<", err)
+		return
+	}
+}
+
+// DungeonRequestDataToRecord -
+func (rnr *Runner) DungeonRequestDataToRecord(data schema.DungeonData, rec *record.Dungeon) error {
 
 	return nil
 }
 
-// RecordToGameResponseData -
-func (rnr *Runner) RecordToGameResponseData(gameRec *record.Game) (schema.GameData, error) {
+// RecordToDungeonResponseData -
+func (rnr *Runner) RecordToDungeonResponseData(gameRec *record.Dungeon) (schema.DungeonData, error) {
 
-	data := schema.GameData{
+	data := schema.DungeonData{
 		ID:        gameRec.ID,
 		CreatedAt: gameRec.CreatedAt,
 		UpdatedAt: gameRec.UpdatedAt.Time,
