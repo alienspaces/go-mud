1d47e1b93eb1cb0e6e751d6acd457251746923fc
alienspaces <alienspaces@gmail.com>
2021-10-29 19:57:51 +1100

    Add action and location to character action response

--- server/service/game/internal/server/runner/dungeoncharacteractionshandler.go
+++ server/service/game/internal/server/runner/dungeoncharacteractionshandler.go
@@ -60,9 +60,60 @@ func (rnr *Runner) PostDungeonCharacterActionsHandler(w http.ResponseWriter, r *
 // RecordToCharacterResponseData -
 func (rnr *Runner) RecordToDungeonCharacterActionResponseData(dungeonActionRecordSet *model.DungeonActionRecordSet) (schema.DungeonActionResponseData, error) {
 
+	dungeonActionRec := dungeonActionRecordSet.DungeonActionRec
+	dungeonLocationRec := dungeonActionRecordSet.DungeonLocationRec
+
+	dungeonLocations := []string{}
+	if dungeonLocationRec.NorthDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "north")
+	}
+	if dungeonLocationRec.NortheastDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "northeast")
+	}
+	if dungeonLocationRec.EastDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "east")
+	}
+	if dungeonLocationRec.SoutheastDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "southeast")
+	}
+	if dungeonLocationRec.SouthDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "south")
+	}
+	if dungeonLocationRec.SouthwestDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "southwest")
+	}
+	if dungeonLocationRec.SouthwestDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "southwest")
+	}
+	if dungeonLocationRec.WestDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "west")
+	}
+	if dungeonLocationRec.NorthwestDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "northwest")
+	}
+	if dungeonLocationRec.UpDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "up")
+	}
+	if dungeonLocationRec.DownDungeonLocationID.Valid {
+		dungeonLocations = append(dungeonLocations, "down")
+	}
+
 	data := schema.DungeonActionResponseData{
 		Action: schema.ActionData{
-			ID: dungeonActionRecordSet.DungeonActionRec.ID,
+			ID:                             dungeonActionRec.ID,
+			Command:                        dungeonActionRec.ResolvedCommand,
+			EquippedDungeonObjectName:      dungeonActionRec.ResolvedEquippedDungeonObjectName.String,
+			StashedDungeonObjectName:       dungeonActionRec.ResolvedStashedDungeonObjectName.String,
+			TargetDungeonObjectName:        dungeonActionRec.ResolvedTargetDungeonObjectName.String,
+			TargetDungeonCharacterName:     dungeonActionRec.ResolvedTargetDungeonCharacterName.String,
+			TargetDungeonMonsterName:       dungeonActionRec.ResolvedTargetDungeonMonsterName.String,
+			TargetDungeonLocationName:      dungeonActionRec.ResolvedTargetDungeonLocationName.String,
+			TargetDungeonLocationDirection: dungeonActionRec.ResolvedTargetDungeonLocationDirection.String,
+		},
+		Location: schema.LocationData{
+			Name:        dungeonLocationRec.Name,
+			Description: dungeonLocationRec.Description,
+			Directions:  dungeonLocations,
 		},
 	}
 
