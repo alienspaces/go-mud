0a3e6266ef9124bab884e4f45cc8f328d0161740
alienspaces <alienspaces@gmail.com>
2021-11-27 19:57:56 +1100

    Extract common handler test code into a reusable test function

--- server/service/game/internal/server/runner/dungeoncharacteractionshandler_test.go
+++ server/service/game/internal/server/runner/dungeoncharacteractionshandler_test.go
@@ -1,18 +1,13 @@
 package runner
 
 import (
-	"bytes"
 	"encoding/json"
-	"fmt"
+	"io"
 	"net/http"
-	"net/http/httptest"
-	"net/url"
-	"strings"
 	"testing"
 
 	"gitlab.com/alienspaces/go-mud/server/schema"
 
-	"github.com/julienschmidt/httprouter"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/alienspaces/go-mud/server/core/auth"
 	"gitlab.com/alienspaces/go-mud/server/core/server"
@@ -25,15 +20,10 @@ func TestDungeonCharacterActionHandler(t *testing.T) {
 	th, err := NewTestHarness()
 	require.NoError(t, err, "New test data returns without error")
 
-	type TestCase struct {
-		name               string
-		config             func(rnr *Runner) server.HandlerConfig
-		requestHeaders     func(data harness.Data) map[string]string
-		requestParams      func(data harness.Data) map[string]string
-		queryParams        func(data harness.Data) map[string]string
-		requestBody        func(data harness.Data) *schema.DungeonActionRequest
+	type testCase struct {
+		TestCase
+		// requestBody        func(data harness.Data) *schema.DungeonActionRequest
 		expectResponseBody func(data harness.Data) *schema.DungeonActionResponse
-		expectResponseCode int
 	}
 
 	// validAuthToken - Generate a valid authentication token for this handler
@@ -46,33 +36,44 @@ func TestDungeonCharacterActionHandler(t *testing.T) {
 		return token
 	}
 
-	tests := []TestCase{
+	testCases := []testCase{
 		{
-			name: "POST - look",
-			config: func(rnr *Runner) server.HandlerConfig {
-				return rnr.HandlerConfig[6]
+			TestCase: TestCase{
+				Name: "POST - look",
+				HandlerConfig: func(rnr *Runner) server.HandlerConfig {
+					return rnr.HandlerConfig[6]
+				},
+				RequestHeaders: func(data harness.Data) map[string]string {
+					headers := map[string]string{
+						"Authorization": "Bearer " + validAuthToken(),
+					}
+					return headers
+				},
+				RequestPathParams: func(data harness.Data) map[string]string {
+					params := map[string]string{
+						":dungeon_id":   data.DungeonRecs[0].ID,
+						":character_id": data.DungeonCharacterRecs[0].ID,
+					}
+					return params
+				},
+				RequestBody: func(data harness.Data) interface{} {
+					res := schema.DungeonActionRequest{
+						Data: schema.DungeonActionRequestData{
+							Sentence: "look",
+						},
+					}
+					return &res
+				},
+				ResponseCode: http.StatusOK,
 			},
-			requestHeaders: func(data harness.Data) map[string]string {
-				headers := map[string]string{
-					"Authorization": "Bearer " + validAuthToken(),
-				}
-				return headers
-			},
-			requestParams: func(data harness.Data) map[string]string {
-				params := map[string]string{
-					":dungeon_id":   data.DungeonRecs[0].ID,
-					":character_id": data.DungeonCharacterRecs[0].ID,
-				}
-				return params
-			},
-			requestBody: func(data harness.Data) *schema.DungeonActionRequest {
-				res := schema.DungeonActionRequest{
-					Data: schema.DungeonActionRequestData{
-						Sentence: "look",
-					},
-				}
-				return &res
-			},
+			// requestBody: func(data harness.Data) *schema.DungeonActionRequest {
+			// 	res := schema.DungeonActionRequest{
+			// 		Data: schema.DungeonActionRequestData{
+			// 			Sentence: "look",
+			// 		},
+			// 	}
+			// 	return &res
+			// },
 			expectResponseBody: func(data harness.Data) *schema.DungeonActionResponse {
 				res := schema.DungeonActionResponse{
 					Data: []schema.DungeonActionResponseData{
@@ -86,33 +87,35 @@ func TestDungeonCharacterActionHandler(t *testing.T) {
 				}
 				return &res
 			},
-			expectResponseCode: http.StatusOK,
 		},
 		{
-			name: "POST - move north",
-			config: func(rnr *Runner) server.HandlerConfig {
-				return rnr.HandlerConfig[6]
-			},
-			requestHeaders: func(data harness.Data) map[string]string {
-				headers := map[string]string{
-					"Authorization": "Bearer " + validAuthToken(),
-				}
-				return headers
-			},
-			requestParams: func(data harness.Data) map[string]string {
-				params := map[string]string{
-					":dungeon_id":   data.DungeonRecs[0].ID,
-					":character_id": data.DungeonCharacterRecs[0].ID,
-				}
-				return params
-			},
-			requestBody: func(data harness.Data) *schema.DungeonActionRequest {
-				res := schema.DungeonActionRequest{
-					Data: schema.DungeonActionRequestData{
-						Sentence: "move north",
-					},
-				}
-				return &res
+			TestCase: TestCase{
+				Name: "POST - move north",
+				HandlerConfig: func(rnr *Runner) server.HandlerConfig {
+					return rnr.HandlerConfig[6]
+				},
+				RequestHeaders: func(data harness.Data) map[string]string {
+					headers := map[string]string{
+						"Authorization": "Bearer " + validAuthToken(),
+					}
+					return headers
+				},
+				RequestPathParams: func(data harness.Data) map[string]string {
+					params := map[string]string{
+						":dungeon_id":   data.DungeonRecs[0].ID,
+						":character_id": data.DungeonCharacterRecs[0].ID,
+					}
+					return params
+				},
+				RequestBody: func(data harness.Data) interface{} {
+					res := schema.DungeonActionRequest{
+						Data: schema.DungeonActionRequestData{
+							Sentence: "move north",
+						},
+					}
+					return &res
+				},
+				ResponseCode: http.StatusOK,
 			},
 			expectResponseBody: func(data harness.Data) *schema.DungeonActionResponse {
 				res := schema.DungeonActionResponse{
@@ -127,154 +130,56 @@ func TestDungeonCharacterActionHandler(t *testing.T) {
 				}
 				return &res
 			},
-			expectResponseCode: http.StatusOK,
 		},
-		// {
-		// 	name: "POST - empty",
-		// 	config: func(rnr *Runner) server.HandlerConfig {
-		// 		return rnr.HandlerConfig[6]
-		// 	},
-		// 	requestHeaders: func(data harness.Data) map[string]string {
-		// 		headers := map[string]string{
-		// 			"Authorization": "Bearer " + validAuthToken(),
-		// 		}
-		// 		return headers
-		// 	},
-		// 	requestParams: func(data harness.Data) map[string]string {
-		// 		params := map[string]string{
-		// 			":dungeon_id":   data.DungeonRecs[0].ID,
-		// 			":character_id": data.DungeonCharacterRecs[0].ID,
-		// 		}
-		// 		return params
-		// 	},
-		// 	requestBody: func(data harness.Data) *schema.DungeonActionRequest {
-		// 		res := schema.DungeonActionRequest{
-		// 			Data: schema.DungeonActionRequestData{
-		// 				Sentence: "",
-		// 			},
-		// 		}
-		// 		return &res
-		// 	},
-		// 	responseCode: http.StatusBadRequest,
-		// },
+		{
+			TestCase: TestCase{
+				Name: "POST - empty",
+				HandlerConfig: func(rnr *Runner) server.HandlerConfig {
+					return rnr.HandlerConfig[6]
+				},
+				RequestHeaders: func(data harness.Data) map[string]string {
+					headers := map[string]string{
+						"Authorization": "Bearer " + validAuthToken(),
+					}
+					return headers
+				},
+				RequestPathParams: func(data harness.Data) map[string]string {
+					params := map[string]string{
+						":dungeon_id":   data.DungeonRecs[0].ID,
+						":character_id": data.DungeonCharacterRecs[0].ID,
+					}
+					return params
+				},
+				RequestBody: func(data harness.Data) interface{} {
+					res := schema.DungeonActionRequest{
+						Data: schema.DungeonActionRequestData{
+							Sentence: "",
+						},
+					}
+					return &res
+				},
+				ResponseCode: http.StatusBadRequest,
+			},
+		},
 	}
 
-	for _, tc := range tests {
+	for _, testCase := range testCases {
 
-		t.Logf("Running test >%s<", tc.name)
+		t.Logf("Running test >%s<", testCase.Name)
 
-		func() {
-			rnr := NewRunner()
+		testFunc := func(method string, body io.Reader) {
 
-			err = rnr.Init(th.Config, th.Log, th.Store)
-			require.NoError(t, err, "Runner init returns without error")
-
-			err = th.Setup()
-			require.NoError(t, err, "Test data setup returns without error")
-			defer func() {
-				err = th.Teardown()
-				require.NoError(t, err, "Test data teardown returns without error")
-			}()
-
-			// config
-			cfg := tc.config(rnr)
-
-			// handler
-			h, _ := rnr.DefaultMiddleware(cfg, cfg.HandlerFunc)
-
-			// router
-			rtr := httprouter.New()
-
-			switch cfg.Method {
-			case http.MethodGet:
-				rtr.GET(cfg.Path, h)
-			case http.MethodPost:
-				rtr.POST(cfg.Path, h)
-			case http.MethodPut:
-				rtr.PUT(cfg.Path, h)
-			case http.MethodDelete:
-				rtr.DELETE(cfg.Path, h)
-			default:
-				//
-			}
-
-			// request params
-			requestParams := map[string]string{}
-			if tc.requestParams != nil {
-				requestParams = tc.requestParams(th.Data)
-			}
-
-			requestPath := cfg.Path
-			for paramKey, paramValue := range requestParams {
-				requestPath = strings.Replace(requestPath, paramKey, paramValue, 1)
-			}
-
-			// query params
-			queryParams := map[string]string{}
-			if tc.queryParams != nil {
-				queryParams = tc.queryParams(th.Data)
-			}
-
-			if len(queryParams) > 0 {
-				count := 0
-				for paramKey, paramValue := range queryParams {
-					if count == 0 {
-						requestPath = requestPath + `?`
-					} else {
-						requestPath = requestPath + `&`
-					}
-					t.Logf("Adding parameter key >%s< param >%s<", paramKey, paramValue)
-					requestPath = fmt.Sprintf("%s%s=%s", requestPath, paramKey, url.QueryEscape(paramValue))
-				}
-				t.Logf("Resulting requestPath >%s<", requestPath)
-			}
-
-			// request data
-			data := tc.requestBody(th.Data)
-
-			var req *http.Request
-
-			if data != nil {
-				jsonData, err := json.Marshal(data)
-				require.NoError(t, err, "Marshal returns without error")
-
-				req, err = http.NewRequest(cfg.Method, requestPath, bytes.NewBuffer(jsonData))
-				require.NoError(t, err, "NewRequest returns without error")
-			} else {
-				req, err = http.NewRequest(cfg.Method, requestPath, nil)
-				require.NoError(t, err, "NewRequest returns without error")
-			}
-
-			// request headers
-			requestHeaders := map[string]string{}
-			if tc.requestHeaders != nil {
-				requestHeaders = tc.requestHeaders(th.Data)
-			}
-
-			for headerKey, headerVal := range requestHeaders {
-				req.Header.Add(headerKey, headerVal)
-			}
-
-			// recorder
-			rec := httptest.NewRecorder()
-
-			// serve
-			rtr.ServeHTTP(rec, req)
-
-			// test status
-			require.Equalf(t, tc.expectResponseCode, rec.Code, "%s - Response code equals expected", tc.name)
-
-			responseBody := schema.DungeonActionResponse{}
-			err = json.NewDecoder(rec.Body).Decode(&responseBody)
+			var responseBody *schema.DungeonActionResponse
+			err = json.NewDecoder(body).Decode(&responseBody)
 			require.NoError(t, err, "Decode returns without error")
 
-			if tc.expectResponseCode != http.StatusOK {
+			if testCase.TestResponseCode() != http.StatusOK {
 				return
 			}
 
 			// test data
-			if tc.expectResponseBody != nil {
-				expectResponseBody := tc.expectResponseBody(th.Data)
+			if testCase.expectResponseBody != nil {
+				expectResponseBody := testCase.expectResponseBody(th.Data)
 				if expectResponseBody != nil {
 					for idx, expectData := range expectResponseBody.Data {
 						require.NotNil(t, responseBody.Data[idx], "Response body index is not empty")
@@ -291,20 +196,22 @@ func TestDungeonCharacterActionHandler(t *testing.T) {
 
 				// record timestamps
 				require.False(t, data.Action.CreatedAt.IsZero(), "CreatedAt is not zero")
-				if cfg.Method == http.MethodPost {
+				if method == http.MethodPost {
 					require.True(t, data.Action.UpdatedAt.IsZero(), "UpdatedAt is zero")
 				}
-				if cfg.Method == http.MethodPut {
+				if method == http.MethodPut {
 					require.False(t, data.Action.UpdatedAt.IsZero(), "UpdatedAt is not zero")
 				}
 
-				if cfg.Method == http.MethodPost {
+				if method == http.MethodPost {
 					if len(responseBody.Data) != 0 {
 						t.Logf("Adding dungeon character action ID >%s< for teardown", data.Action.ID)
 						th.AddDungeonCharacterActionTeardownID(data.Action.ID)
 					}
 				}
 			}
-		}()
+		}
+
+		RunTestCase(t, th, &testCase, testFunc)
 	}
 }
--- server/service/game/internal/server/runner/handler_test.go
+++ server/service/game/internal/server/runner/handler_test.go
@@ -1,1 +1,176 @@
 package runner
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/require"
+	"gitlab.com/alienspaces/go-mud/server/core/server"
+	"gitlab.com/alienspaces/go-mud/server/service/game/internal/harness"
+)
+
+type TestCaser interface {
+	TestName() string
+	TestHandlerConfig(rnr *Runner) server.HandlerConfig
+	TestRequestHeaders(data harness.Data) map[string]string
+	TestRequestPathParams(data harness.Data) map[string]string
+	TestRequestQueryParams(data harness.Data) map[string]string
+	TestRequestBody(data harness.Data) interface{}
+	TestResponseCode() int
+}
+
+type TestCase struct {
+	Name               string
+	HandlerConfig      func(rnr *Runner) server.HandlerConfig
+	RequestHeaders     func(data harness.Data) map[string]string
+	RequestPathParams  func(data harness.Data) map[string]string
+	RequestQueryParams func(data harness.Data) map[string]string
+	RequestBody        func(data harness.Data) interface{}
+	ResponseCode       int
+}
+
+//lint:ignore U1000 - testing struct implements interface
+var _testCase TestCaser = &TestCase{}
+
+func (t *TestCase) TestName() string {
+	return t.Name
+}
+
+func (t *TestCase) TestHandlerConfig(rnr *Runner) server.HandlerConfig {
+	return t.HandlerConfig(rnr)
+}
+
+func (t *TestCase) TestRequestHeaders(data harness.Data) map[string]string {
+	return t.RequestHeaders(data)
+}
+
+func (t *TestCase) TestRequestPathParams(data harness.Data) map[string]string {
+	pp := map[string]string{}
+	if t.RequestPathParams != nil {
+		pp = t.RequestPathParams(data)
+	}
+	return pp
+}
+
+func (t *TestCase) TestRequestQueryParams(data harness.Data) map[string]string {
+	qp := map[string]string{}
+	if t.RequestQueryParams != nil {
+		qp = t.RequestQueryParams(data)
+	}
+	return qp
+}
+
+func (t *TestCase) TestRequestBody(data harness.Data) interface{} {
+	var b interface{}
+	if t.RequestBody != nil {
+		b = t.RequestBody(data)
+	}
+	return b
+}
+
+func (t *TestCase) TestResponseCode() int {
+	return t.ResponseCode
+}
+
+func RunTestCase(t *testing.T, th *harness.Testing, testCase TestCaser, testFunc func(method string, body io.Reader)) {
+	rnr := NewRunner()
+
+	err := rnr.Init(th.Config, th.Log, th.Store)
+	require.NoError(t, err, "Runner init returns without error")
+
+	err = th.Setup()
+	require.NoError(t, err, "Test data setup returns without error")
+	defer func() {
+		err = th.Teardown()
+		require.NoError(t, err, "Test data teardown returns without error")
+	}()
+
+	// config
+	cfg := testCase.TestHandlerConfig(rnr)
+
+	// handler
+	h, _ := rnr.DefaultMiddleware(cfg, cfg.HandlerFunc)
+
+	// router
+	rtr := httprouter.New()
+
+	switch cfg.Method {
+	case http.MethodGet:
+		rtr.GET(cfg.Path, h)
+	case http.MethodPost:
+		rtr.POST(cfg.Path, h)
+	case http.MethodPut:
+		rtr.PUT(cfg.Path, h)
+	case http.MethodDelete:
+		rtr.DELETE(cfg.Path, h)
+	default:
+		//
+	}
+
+	// request params
+	requestParams := testCase.TestRequestPathParams(th.Data)
+
+	requestPath := cfg.Path
+	for paramKey, paramValue := range requestParams {
+		requestPath = strings.Replace(requestPath, paramKey, paramValue, 1)
+	}
+
+	// query params
+	queryParams := testCase.TestRequestQueryParams(th.Data)
+
+	if len(queryParams) > 0 {
+		count := 0
+		for paramKey, paramValue := range queryParams {
+			if count == 0 {
+				requestPath = requestPath + `?`
+			} else {
+				requestPath = requestPath + `&`
+			}
+			t.Logf("Adding parameter key >%s< param >%s<", paramKey, paramValue)
+			requestPath = fmt.Sprintf("%s%s=%s", requestPath, paramKey, url.QueryEscape(paramValue))
+		}
+		t.Logf("Resulting requestPath >%s<", requestPath)
+	}
+
+	// request data
+	data := testCase.TestRequestBody(th.Data)
+
+	var req *http.Request
+
+	if data != nil {
+		jsonData, err := json.Marshal(data)
+		require.NoError(t, err, "Marshal returns without error")
+
+		req, err = http.NewRequest(cfg.Method, requestPath, bytes.NewBuffer(jsonData))
+		require.NoError(t, err, "NewRequest returns without error")
+	} else {
+		req, err = http.NewRequest(cfg.Method, requestPath, nil)
+		require.NoError(t, err, "NewRequest returns without error")
+	}
+
+	// request headers
+	requestHeaders := testCase.TestRequestHeaders(th.Data)
+
+	for headerKey, headerVal := range requestHeaders {
+		req.Header.Add(headerKey, headerVal)
+	}
+
+	// recorder
+	rec := httptest.NewRecorder()
+
+	// serve
+	rtr.ServeHTTP(rec, req)
+
+	// test status
+	require.Equalf(t, testCase.TestResponseCode(), rec.Code, "%s - Response code equals expected", testCase.TestName())
+
+	testFunc(cfg.Method, rec.Body)
+}
